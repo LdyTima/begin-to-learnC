ff3dfb9b06b87e377e8f4bc73adf64388cb0c8d6
2294868333 <72567204+2294868333@users.noreply.github.com>
2020-10-10 08:14:03 +0800

    变量的使用
    变量的使用例子

---
+++ 变量的使用.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	//变量
+	//意义：方便管理内存空间
+	//语法： 数据类型 变量名 = 变量初始值 ；
+
+	int a = 10;
+	cout << "a = " << a << endl;
+
+	system("pause");
+	return 0;
+}
